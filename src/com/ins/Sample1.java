80d2ab9c0e69c4e712f98efb2aef4d3c9f08bd7b
RudUnt <rudreshuntwal26@gmail.com>
2024-10-22 14:42:28 +0530

    Initial commit to GitPush2

---
+++ src/com/ins/Sample1.java
@@ -0,0 +1,7 @@
+package com.ins;
+
+public class Sample1 {
+	public static void main(String[] args) {
+		System.out.println("I am the Sample1 from GitPushTest2");
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module GitPushTest2 {
+}
